063f86aaf2ef5ad81c0b6e9d4cea7da9481cbc37
Edwin Wallin <edwin@dequilla.com>
2018-10-12 16:49:34 +0200

    Update install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,4 @@
-mkdir ~/.vim/autoload/
+mkdir -p ~/.vim/autoload/
 curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim --output ~/.vim/autoload/plug.vim
 
 curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/Dequilla/configs/master/vim/.vimrc --output ~/.vimrc
