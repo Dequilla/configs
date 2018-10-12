e06f372b94657059934eab4200e0e8857735ba89
Edwin Wallin <edwin@dequilla.com>
2018-10-12 16:32:27 +0200

    Update install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,3 @@
-curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim --output ~/vimfiles/autoload
+curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim --output ~/.vim/autoload
 
 curl https://raw.githubusercontent.com/Dequilla/configs/master/vim/.vimrc --output ~/.vimrc
